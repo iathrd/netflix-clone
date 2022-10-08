88bfb52d9326d80d578bd1ce03832b7133a03b2a
Iqbal Athorid <iqbalathoridd@gmail.com>
2022-10-08 13:57:31 +0700

    created Header components

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,29 @@
+import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
 function Header() {
+    const [isScrolled,setIsScrolled] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if(window.scrollY > 0) {
+                setIsScrolled(true)
+            } else{ 
+                setIsScrolled(false)
+            }
+        }
+
+        window.addEventListener('scroll',handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll',handleScroll)
+        }
+
+    },[])
+
   return (
-    <header>
+    <header className={`${isScrolled && "bg-red-500"}`}>
         <div className="flex items-center space-x-2 md:space-x-10">
             <img
             src="https://rb.gy/ulxxee"
@@ -15,6 +38,19 @@ function Header() {
                 <li className="headerLink">New & Pipular</li>
                 <li className="headerLink">My List</li>
             </ul>
+        </div>
+
+        <div className='flex items-center space-x-4 text-sm font-light'>
+            <MagnifyingGlassIcon className='hidden sm:inline h-6 w-6'/>
+            <p className='hidden lg:inline' >Kids</p>
+            <BellIcon className='h-6 w-6'/>
+            <Link href="/account">
+                <img
+                src='https://rb.gy/g1pwyx'
+                alt=''
+                className='cursor-pointer rounded'
+                />
+            </Link>
         </div>
     </header>
   )
