a0754b09b5f16184507748682cbaf98bcd6534a6
Iqbal Athorid <iqbalathoridd@gmail.com>
2022-11-02 14:09:25 +0700

    craete Banner component

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,16 +1,44 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import { Movie } from "../typings";
+import {baseUrl} from '../constans/movie';
+import {FaPlay} from 'react-icons/fa'
+import { InformationCircleIcon } from "@heroicons/react/24/solid";
 
 type Props = {
     netflixOriginals:Movie[]
 }
 
 function Banner({netflixOriginals}:Props) {
+  const [movie,setMovie] = useState<Movie | null>(null)
+  console.log(netflixOriginals)
+
+  useEffect(() => {
+    setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)])
+  },[netflixOriginals])
+
   return (
-    <div>
-        <div>
-            {/* <Image/> */}
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-center" >
+        <div className="absolute top-0 -z-10 left-0 h-[95vh] w-screen" >
+            <Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+            layout="fill"
+            objectFit="cover"
+             />
         </div>
+        <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
+          {movie?.title || movie?.name || movie?.original_name}
+          </h1>
+        <p className="max-w-xs text-shadow text-xs md:max-w-lg lg:text-lg lg:max-w-2xl">
+          {movie?.overview}
+          </p>
+        <div className="flex space-x-3">
+          <button className="bannerButton bg-white text-black" >
+             <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7"/> Play
+             </button>
+          <button className="bannerButton bg-[gray]/70">
+            More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
+            </button>
+        </div> 
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -28,13 +28,13 @@ const Home = ({
 }:Props) => {
   return (
     <div className='relative h-screen bg-gradient-to-b from-gray-900/10
-    to-[#010511] lg:h-[140vh]'>
+    to-[#010511] lg:h-[140vh] overflow-x-hidden'>
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
-      <main>
+      <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
         <Banner netflixOriginals={netflixOriginals} />
       </main>
     </div>
